6c41d2a33aae050c77db04596bc142f1132c7f61
davidfung <davidfung@amgcomputing.com>
2019-12-12 23:35:17 -0800

    Change TaskView to a statefulwidget.

--- lib/main.dart
+++ lib/main.dart
@@ -35,43 +35,58 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class TaskView extends StatelessWidget {
+class TaskView extends StatefulWidget {
+  @override
+  _TaskViewState createState() => _TaskViewState();
+}
+
+class _TaskViewState extends State<TaskView> {
+  final items = List<String>.generate(26, (i) => 'Entry ${i + 1}');
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        TaskItem(name: 'Entry A'),
-        TaskItem(name: 'Entry B'),
-        TaskItem(name: 'Entry C'),
-        TaskItem(name: 'Entry D'),
-        TaskItem(name: 'Entry E'),
-        TaskItem(name: 'Entry F'),
-        TaskItem(name: 'Entry G'),
-        TaskItem(name: 'Entry H'),
-        TaskItem(name: 'Entry I'),
-        TaskItem(name: 'Entry J'),
-        TaskItem(name: 'Entry K'),
-        TaskItem(name: 'Entry L'),
-        TaskItem(name: 'Entry M'),
-        TaskItem(name: 'Entry N'),
-        TaskItem(name: 'Entry O'),
-        TaskItem(name: 'Entry P'),
-        TaskItem(name: 'Entry Q'),
-        TaskItem(name: 'Entry R'),
-        TaskItem(name: 'Entry S'),
-        TaskItem(name: 'Entry T'),
-        TaskItem(name: 'Entry U'),
-        TaskItem(name: 'Entry V'),
-        TaskItem(name: 'Entry W'),
-        TaskItem(name: 'Entry X'),
-        TaskItem(name: 'Entry Y'),
-        TaskItem(name: 'Entry Z'),
-      ],
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return ListTile(title: Text('${items[index]}'));
+      },
     );
   }
 }
 
+/*
+return ListView(
+padding: const EdgeInsets.all(8),
+children: <Widget>[
+TaskItem(name: 'Entry A'),
+TaskItem(name: 'Entry B'),
+TaskItem(name: 'Entry C'),
+TaskItem(name: 'Entry D'),
+TaskItem(name: 'Entry E'),
+TaskItem(name: 'Entry F'),
+TaskItem(name: 'Entry G'),
+TaskItem(name: 'Entry H'),
+TaskItem(name: 'Entry I'),
+TaskItem(name: 'Entry J'),
+TaskItem(name: 'Entry K'),
+TaskItem(name: 'Entry L'),
+TaskItem(name: 'Entry M'),
+TaskItem(name: 'Entry N'),
+TaskItem(name: 'Entry O'),
+TaskItem(name: 'Entry P'),
+TaskItem(name: 'Entry Q'),
+TaskItem(name: 'Entry R'),
+TaskItem(name: 'Entry S'),
+TaskItem(name: 'Entry T'),
+TaskItem(name: 'Entry U'),
+TaskItem(name: 'Entry V'),
+TaskItem(name: 'Entry W'),
+TaskItem(name: 'Entry X'),
+TaskItem(name: 'Entry Y'),
+TaskItem(name: 'Entry Z'),
+],
+);
+*/
 class TaskTile extends StatelessWidget {
   final String name;
 
