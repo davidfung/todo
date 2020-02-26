41baaad6aa14e9f5eefec1ed149a32a9a5797e45
davidfung <davidfung@amgcomputing.com>
2020-02-26 00:06:31 -0800

    Building the Settings page (word in progress)

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,23 +1,54 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todo/constants.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   static const String routeName = '/settings';
   static const String title = 'Settings';
 
   @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(SettingsPage.title),
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Text('Settings Page'),
-          ],
+      body: _buildPage(context),
+    );
+  }
+
+  Widget _buildPage(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ListTile(
+            title: const Padding(
+                padding: EdgeInsets.only(left: 10.0, top: 10.0),
+                child: Text(
+                  'Email Recipients',
+                  style: captionStyle,
+                ))),
+        _buildEmailRecipient(context, 1),
+        ListTile(
+          title: const Text('6 digits'),
+          leading: Radio(),
         ),
-      ),
+        ListTile(
+          title: const Text('8 digits'),
+          leading: Radio(),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildEmailRecipient(BuildContext context, int index) {
+    return Row(
+      children: <Widget>[
+        Text(index.toString()),
+      ],
     );
   }
 }
