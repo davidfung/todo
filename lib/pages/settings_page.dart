1179d1b82c13e94dfcb9140f259f617fb6b6cf8b
davidfung <davidfung@amgcomputing.com>
2020-02-29 21:39:52 -0800

    Add two more email recipient settings

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -16,24 +16,90 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   String email1 = "";
+  String email2 = "";
+  String email3 = "";
   bool to1 = false;
+  bool to2 = false;
+  bool to3 = false;
   bool cc1 = false;
+  bool cc2 = false;
+  bool cc3 = false;
   bool loaded = false;
 
   @override
   void initState() {
-    loadSettings();
+    _loadSettings();
     super.initState();
   }
 
-  Future<void> loadSettings() async {
+  Future<void> _loadSettings() async {
     this.email1 = await loadString(settingEmail1, defaultValue: "");
+    this.email2 = await loadString(settingEmail2, defaultValue: "");
+    this.email3 = await loadString(settingEmail3, defaultValue: "");
     this.to1 = await loadBool(settingTo1, defaultValue: true);
+    this.to2 = await loadBool(settingTo2, defaultValue: true);
+    this.to3 = await loadBool(settingTo3, defaultValue: true);
     this.cc1 = await loadBool(settingCc1, defaultValue: false);
+    this.cc2 = await loadBool(settingCc2, defaultValue: false);
+    this.cc3 = await loadBool(settingCc3, defaultValue: false);
     print("loaded $settingTo1=$to1");
     print("loaded $settingCc1=$cc1");
     loaded = true;
     setState(() {});
+  }
+
+  void _updateEmail(int index, String value) {
+    switch (index) {
+      case 1:
+        this.email1 = value;
+        saveString(settingEmail1, value);
+        break;
+      case 2:
+        this.email2 = value;
+        saveString(settingEmail2, value);
+        break;
+      case 3:
+        this.email3 = value;
+        saveString(settingEmail3, value);
+        break;
+    }
+    print("saving settingEmail$index=$value");
+  }
+
+  void _updateTo(int index, bool value) {
+    switch (index) {
+      case 1:
+        this.to1 = value;
+        saveBool(settingTo1, value);
+        break;
+      case 2:
+        this.to2 = value;
+        saveBool(settingTo2, value);
+        break;
+      case 3:
+        this.to3 = value;
+        saveBool(settingTo3, value);
+        break;
+    }
+    print("saving settingTo$index=$value");
+  }
+
+  void _updateCc(int index, bool value) {
+    switch (index) {
+      case 1:
+        this.cc1 = value;
+        saveBool(settingCc1, value);
+        break;
+      case 2:
+        this.cc2 = value;
+        saveBool(settingCc2, value);
+        break;
+      case 3:
+        this.cc3 = value;
+        saveBool(settingCc3, value);
+        break;
+    }
+    print("saving settingCc$index=$value");
   }
 
   @override
@@ -59,9 +125,9 @@ class _SettingsPageState extends State<SettingsPage> {
       padding: EdgeInsets.all(10.0),
       children: <Widget>[
         _buildSectionHead(context),
-        _buildEmailRecipient(context),
-        //_buildEmailRecipient(context),
-        //_buildEmailRecipient(context),
+        _buildEmailRecipient(context, 1, this.email1, this.to1, this.cc1),
+        _buildEmailRecipient(context, 2, this.email2, this.to2, this.cc2),
+        _buildEmailRecipient(context, 3, this.email3, this.to3, this.cc3),
       ],
     );
   }
@@ -74,9 +140,10 @@ class _SettingsPageState extends State<SettingsPage> {
     ));
   }
 
-  Widget _buildEmailRecipient(BuildContext context) {
+  Widget _buildEmailRecipient(
+      BuildContext context, int index, String email, bool sendTo, bool sendCc) {
     final _teController = TextEditingController();
-    _teController.text = email1;
+    _teController.text = email;
 
     return Column(
       children: <Widget>[
@@ -88,9 +155,7 @@ class _SettingsPageState extends State<SettingsPage> {
                   keyboardType: TextInputType.emailAddress,
                   controller: _teController,
                   onChanged: (value) {
-                    this.email1 = value;
-                    print("saving $settingEmail1=$value");
-                    saveString(settingEmail1, value);
+                    _updateEmail(index, value);
                   }),
             ),
           ),
@@ -100,24 +165,20 @@ class _SettingsPageState extends State<SettingsPage> {
             Flexible(
                 child: SwitchListTile(
               title: const Text(capTo),
-              value: this.to1,
+              value: sendTo,
               onChanged: (bool value) {
                 setState(() {
-                  this.to1 = value;
-                  print("saving $settingTo1=$value");
-                  saveBool(settingTo1, value);
+                  _updateTo(index, value);
                 });
               },
             )),
             Flexible(
                 child: SwitchListTile(
               title: const Text(capCc),
-              value: this.cc1,
+              value: sendCc,
               onChanged: (bool value) {
                 setState(() {
-                  this.cc1 = value;
-                  print("saving $settingCc1=$value");
-                  saveBool(settingCc1, value);
+                  _updateCc(index, value);
                 });
               },
             )),
