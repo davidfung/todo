a896ae4ab98ab0acd5b53fc556bf093c49d18b9e
davidfung <davidfung@amgcomputing.com>
2020-08-16 00:55:08 -0700

    Add header to drawer

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -11,12 +11,32 @@ class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
+      child: ListView(
+//        padding: EdgeInsets.zero,
         children: <Widget>[
           Container(
-            height: 90,
-            color: Theme.of(context).accentColor,
+            height: 60,
+            child: DrawerHeader(
+                child: Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.only(left: 20),
+                    child: Text('Amazing Todo',
+                        style: TextStyle(fontSize: 18, color: Colors.white))),
+                decoration: BoxDecoration(color: Theme.of(context).accentColor),
+                margin: EdgeInsets.all(0.0),
+                padding: EdgeInsets.all(0.0)),
           ),
+//          DrawerHeader(
+//            child: Text(
+//              'Drawer Header',
+//              style: TextStyle(
+//                fontSize: 18,
+//              ),
+//            ),
+//          ), //          Container(
+//            height: 90,
+//            color: Theme.of(context).accentColor,
+//          ),
           buildListTile(SettingsPage.title, Icons.settings, () {
             Navigator.of(context).popAndPushNamed(SettingsPage.routeName);
           }),
